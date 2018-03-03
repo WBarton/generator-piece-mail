8351471d6819ce140bcae60f957a23ed59e98c0c
Will Barton <willbarton@linney.com>
2018-03-03 20:42:28 +0000

    Add basic brand config.

---
+++ app/templates/brand-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "name": "Piece Mail",
+  "base-url": "",
+  "img-url": {
+    "local": "./images",
+    "prod": "<%= imageLocation %>"
+  },
+  "font-stack": "Helvetica, arial, sans-serif",
+  "font-size-base": "14px",
+  "color-text": "#333333",
+  "color-body": "#ffffff",
+  "color-bg": "#f4f4f2",
+  "color-primary": "#da202a",
+  "color-secondary": "#b0cd57",
+  "color-dark": "#da202a",
+  "color-light": "#b0cd57",
+  "link-browser": "",
+  "link-unsubscribe": ""
+};
